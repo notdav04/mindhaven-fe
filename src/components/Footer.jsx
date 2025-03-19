6f6ffb41ac4b0202e14c691bd9b2a2af0af765ea
notdav04 <davide.alonzi26@gmail.com>
2025-03-19 22:22:27 +0100

    footer mobile fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,16 +5,22 @@ const Footer = () => {
     <>
       <Container fluid className="darkbg " style={{ minHeight: "300px" }}>
         <Row className="p-5">
-          <Col xs={6} className="d-flex justify-content-center ">
+          <Col xs={12} md={6} className="d-flex justify-content-center ">
             <div>
               <img src="src/assets/LogoMH.png" alt="" className="d-block" />
               <p className="lightText">
                 © 2025 MindHaven |{" "}
-                <span className="fw-bold">All Rights reserved</span>
+                <span className="fw-bold d-xs-none d-md-inline">
+                  All Rights reserved
+                </span>
               </p>
             </div>
           </Col>
-          <Col className=" footerSecondary footerColMiddleBorder">
+          <Col
+            xs={12}
+            md={6}
+            className=" footerSecondary footerColMiddleBorder"
+          >
             <div className="p-3 pt-2">
               <p className="mb-1 fw-bold fs-5 lightText">Contatti:</p>
               <p className="mb-1">
@@ -27,12 +33,12 @@ const Footer = () => {
               </p>
             </div>
             <div className="ms-3 lightText">
-              <a href="https://github.com/notdav04" className="me-4">
+              {/* <a href="https://github.com/notdav04" className="me-4">
                 <i
                   class="bi bi-github"
                   style={{ color: "#f8f5f1", fontSize: "25px" }}
                 ></i>
-              </a>
+              </a> */}
               <a
                 href="https://www.instagram.com/davide_alonzi/"
                 className="me-4"
@@ -51,7 +57,13 @@ const Footer = () => {
                   style={{ color: "#f8f5f1", fontSize: "25px" }}
                 ></i>
               </a>
-              <a
+              <a href="" className="me-4">
+                <i
+                  class="bi bi-twitter-x"
+                  style={{ color: "#f8f5f1", fontSize: "25px" }}
+                ></i>
+              </a>
+              {/* <a
                 href="https://www.linkedin.com/in/davide-alonzi-65a9a32b6/"
                 className="me-4"
               >
@@ -59,7 +71,7 @@ const Footer = () => {
                   class="bi bi-linkedin"
                   style={{ color: "#f8f5f1", fontSize: "25px" }}
                 ></i>
-              </a>
+              </a> */}
               {/* <a href="">
                 <i
                   class="bi bi-discord"
